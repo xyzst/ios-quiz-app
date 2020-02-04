494eb697eda364114e41b5363da23cc9f00eeca6
Darren Rambaud <xyzst@users.noreply.github.com>
2020-02-03 21:05:10 -0600

    Initialize iOS/Swift application
    Define a few IBActions/UIButtons. Initialize project
    
    Related to: none

---
+++ Quiz App/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Quiz App
+//
+//  Created by Darren Rambaud on 02/03/2020.
+//  Copyright © 2020 Darren Rambaud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var trueButton: UIButton!
+    
+    @IBOutlet weak var falseButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func answerReceived(_ sender: Any) {
+    }
+    
+}
+
