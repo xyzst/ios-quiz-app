c4649859821442a18a679238f46417c1448565ca
Darren Rambaud <xyzst@users.noreply.github.com>
2020-02-08 17:49:37 -0600

    Add UI Elements
    Add UX element to show if the user got the answer correct or incorrect. Add 2 second delay before resetting correct/incorrect answer to default color
    
    Related to: none

--- Quiz App/ViewController.swift
+++ Quiz App/ViewController.swift
@@ -19,12 +19,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     let questions =  [
-        Question(question: "4 + 2 is equal to 6?", answer: true),
-        Question(question: "5 - 3 is greater than 1?", answer: true),
-        Question(question: "3 + 8 is less than 10?", answer: false)
+        Question(question: "A slug's blood is green.", answer: true),
+        Question(question: "Approximately one quarter of human bones are in the feet.", answer: true),
+        Question(question: "The total surface area of two human lungs is approximately 70 square metres.", answer: true),
+        Question(question: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", answer: true),
+        Question(question: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", answer: false),
+        Question(question: "It is illegal to pee in the Ocean in Portugal.", answer: true),
+        Question(question: "You can lead a cow down stairs but not up stairs.", answer: true),
+        Question(question: "Google was originally called 'Backrub'.", answer: true),
+        Question(question: "Buzz Aldrin's mother's maiden name was 'Moon'.", answer: true),
+        Question(question: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", answer: false),
+        Question(question: "No piece of square dry paper can be folded in half more than 7 times.", answer: false),
+        Question(question: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", answer: true)
     ]
     
-    var whichQuestion = 0
+    var currentQuestion = 0
+    
+    var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,28 +45,36 @@ class ViewController: UIViewController {
     @IBAction func answerReceived(_ sender: UIButton) {
         let answer = NSString(string: sender.currentTitle!).boolValue // convert "True" or "False" to boolean data type
         
-        if (whichQuestion < questions.capacity) {
-            if (questions[whichQuestion].isCorrect(answer: answer)) {
-                print("you choose the correct answer!")
+        if (currentQuestion < questions.capacity) {
+            if (questions[currentQuestion].isCorrect(answer: answer)) {
+                sender.backgroundColor = UIColor.green
             } else {
-                print("wrong!!!")
+                sender.backgroundColor = UIColor.red
             }
-            whichQuestion += 1
+            currentQuestion += 1
             updateQuestionUI()
         }
     }
     
     func updateQuestionUI() {
-        if (whichQuestion < questions.capacity) {
-            questionLabel.text = questions[whichQuestion].question
+        if (currentQuestion < questions.capacity) {
+            questionLabel.text = questions[currentQuestion].question
         } else {
             resetUI()
         }
+        
+        timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(delayedEffect), userInfo: nil, repeats: false)
+    
+    }
+    
+    @objc func delayedEffect() {
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
     }
     
     func resetUI() {
-        whichQuestion = 0
-        questionLabel.text = questions[whichQuestion].question
+        currentQuestion = 0
+        questionLabel.text = questions[currentQuestion].question
     }
     
 }
