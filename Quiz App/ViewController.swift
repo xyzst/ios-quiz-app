c641123fe53d3e24ffa782071cea6b31d9884983
Darren Rambaud <xyzst@users.noreply.github.com>
2020-02-08 17:17:53 -0600

    More Refactoring
    Update struct with helper method. Fix issue related to resetting game state (after all questions have been iterated through the label text would show the last question)
    
    Related to: none

--- Quiz App/ViewController.swift
+++ Quiz App/ViewController.swift
@@ -21,44 +21,30 @@ class ViewController: UIViewController {
     struct Question {
         var question = ""
         var answer = false
+        
+        func isCorrect(answer: Bool) -> Bool {
+            return self.answer == answer
+        }
     }
     
-    let qs =  [
+    let questions =  [
         Question(question: "4 + 2 is equal to 6?", answer: true),
         Question(question: "5 - 3 is greater than 1?", answer: true),
         Question(question: "3 + 8 is less than 10?", answer: false)
     ]
     
-//    let questions = [
-//        0: [
-//            "question": "4 + 2 is equal to 6?",
-//            "answer": true
-//        ],
-//        1: [
-//            "question": "5 - 3 is greater than 1?",
-//            "answer": true
-//        ],
-//        2: [
-//            "question": "3 + 8 is less than 10?",
-//            "answer": false
-//        ]
-//    ]
-    
     var whichQuestion = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        questionLabel.text = questions[0]?["question"] as? String
-        questionLabel.text = qs[0].question
+        questionLabel.text = questions[0].question
     }
 
     @IBAction func answerReceived(_ sender: UIButton) {
         let answer = NSString(string: sender.currentTitle!).boolValue // convert "True" or "False" to boolean data type
         
-//        if (whichQuestion < questions.capacity) {
-        if (whichQuestion < qs.capacity) {
-//            if (questions[whichQuestion]?["answer"] as? Bool == answer) {
-            if (qs[whichQuestion].answer == answer) {
+        if (whichQuestion < questions.capacity) {
+            if (questions[whichQuestion].isCorrect(answer: answer)) {
                 print("you choose the correct answer!")
             } else {
                 print("wrong!!!")
@@ -69,14 +55,16 @@ class ViewController: UIViewController {
     }
     
     func updateQuestionUI() {
-//        if (whichQuestion < questions.capacity) {
-        if (whichQuestion < qs.capacity) {
-//            questionLabel.text = questions[whichQuestion]?["question"] as? String
-            questionLabel.text = qs[whichQuestion].question
+        if (whichQuestion < questions.capacity) {
+            questionLabel.text = questions[whichQuestion].question
         } else {
-//            questionLabel.text = "No more questions!"
-            whichQuestion = 0
+            resetUI()
         }
+    }
+    
+    func resetUI() {
+        whichQuestion = 0
+        questionLabel.text = questions[whichQuestion].question
     }
     
 }
